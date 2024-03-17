2e719072645a71a58e392a5dfcfbd519449b48e8
jianglanni <jianglanniovo@gmail.com>
2024-03-16 20:44:05 -0400

    user management

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html"), name="About"), 
     path('contact/', TemplateView.as_view(template_name="Contact.html"), name="Contact"),
+    path('login/', TemplateView.as_view(template_name="index.html")), 
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
